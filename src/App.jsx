0a6a38a6eb23e6036ed44b0240cdfc0f435b649d
tdnallred <tdnallred@gmail.com>
2025-01-30 17:57:52 -0800

    Added json and Selection component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import './App.css'
+import pokemon from './data/pokemon.json'
+import Selection from './components/Selection'
+
+const pokemonArray = pokemon.results
 
 const App = () => {
-  return <></>
+  return (
+    <div className="App">
+      <h1>Pokemon</h1>
+      <h2>Select a pokemon</h2>
+      <Selection pokemon={pokemonArray} />
+    </div>
+  )
 }
 
 export default App
