27affaafb4a1ddf1e31a4cac1d62a5c87465c912
tdnallred <tdnallred@gmail.com>
2025-02-03 09:37:18 -0800

    added styling to the profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,7 +2,7 @@
 const Profile = (props) => {
   return (
     <div className="profile">
-      <h2 className="pokeNum lato-black">#{props.pokedex}</h2>
+      <h2 className="pokeNum lato-black-italic">{props.pokedex}</h2>
       <img className="character" src={props.CharacterImage} alt="" />
       <h2 className="pokeName lato-black">{props.name}</h2>
     </div>
