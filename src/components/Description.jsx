79dcb88b5023e7bf292c35b7968ddae5970d16bf
tdnallred <tdnallred@gmail.com>
2025-02-02 17:30:36 -0800

    adding styling to the card

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,10 +1,10 @@
 const Description = (props) => {
   return (
     <div>
-      <h2 className="detName">
+      <h2 className="detName lato-black">
         #{props.pokedex} {props.name}
       </h2>
-      <p>{props.description}</p>
+      <p className="lato-bold description">{props.description}</p>
     </div>
   )
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,13 @@
 const Profile = (props) => {
   return (
     <div className="profile">
-      <h2 className="pokeNum">#{props.pokedex}</h2>
+      <h2 className="pokeNum" className="lato-black">
+        #{props.pokedex}
+      </h2>
       <img className="character" src={props.CharacterImage} alt="" />
-      <h2 className="pokeName">{props.name}</h2>
+      <h2 className="pokeName" className="lato-black">
+        {props.name}
+      </h2>
     </div>
   )
 }
